e09224568f81ab0c074b91d4d8d42c61788b6878
Hobbis <21163@fraser.school.nz>
2024-03-28 12:34:29 +1300

    28/03/2024

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
- 
+yes = ('yes', 'y', 'ye', 'yeah', 'yea', 'yah', 'ya', 'yep', 'yeas')
+
+#User Instructions
+if (y_n := input('Would you like to view the program instructions? ')) in yes:
+    print('Placeholder, will add instructions once code is complete.')
+
+
